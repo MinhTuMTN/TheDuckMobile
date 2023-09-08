e1c2a1e440d7c9f08c0a97792fb9e621b1dc3738
DiDi0703 <110220867+DiDi0703@users.noreply.github.com>
2023-09-08 10:34:54 +0700

    Update datacontext

--- TheDuckMobile_WebAPI/Entities/Store.cs
+++ TheDuckMobile_WebAPI/Entities/Store.cs
@@ -1,6 +1,22 @@
-﻿namespace TheDuckMobile_WebAPI.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheDuckMobile_WebAPI.Entities
 {
     public class Store
     {
+        [Key]
+        public Guid StoreId { get; set; }
+        public string StoreName { get; set; }
+        public DateTime OpenHours { get; set; }
+        public DateTime CreatAt { get; set; }
+        public DateTime LastModifiedAt { get; set;}
+        public bool IsDeleted { get; set; }
+
+        //Quan hệ 1 - 1 với bảng Address
+        public Guid AddressId { get; set; }
+        public Address Address { get; set; }
+
+
+
     }
 }
