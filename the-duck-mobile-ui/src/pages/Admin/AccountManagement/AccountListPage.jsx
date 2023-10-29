a0ed6b3ae41c23e39510cac89d63c65da004b139
Quoc Bao <nguyenquocbaost.2002@gmail.com>
2023-10-29 09:59:00 +0700

    Add search bar to table in admin list pages

--- the-duck-mobile-ui/src/pages/Admin/AccountManagement/AccountListPage.jsx
+++ the-duck-mobile-ui/src/pages/Admin/AccountManagement/AccountListPage.jsx
@@ -1,5 +1,6 @@
 import {
     Box,
+    InputAdornment,
     Paper,
     Table,
     TableBody,
@@ -13,11 +14,40 @@ import {
     styled
 } from "@mui/material";
 import TablePaginationActions from "../../../components/TablePaginationActions";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MuiButton from "../../../components/MuiButton";
 import InfoIcon from '@mui/icons-material/Info';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import MuiTextFeild from "../../../components/MuiTextFeild";
+import { Search } from "@mui/icons-material";
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich1', 237, 9.0, 37, 4.3),
+    createData('Eclair2', 262, 16.0, 24, 6.0),
+    createData('Cupcake3', 305, 3.7, 67, 4.3),
+    createData('Gingerbread3', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt4', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich5', 237, 9.0, 37, 4.3),
+    createData('Eclair6', 262, 16.0, 24, 6.0),
+    createData('Cupcake7', 305, 3.7, 67, 4.3),
+    createData('Gingerbread8', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt9', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich0', 237, 9.0, 37, 4.3),
+    createData('Eclair11', 262, 16.0, 24, 6.0),
+    createData('Cupcake12', 305, 3.7, 67, 4.3),
+    createData('Gingerbread13', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt14', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich15', 237, 9.0, 37, 4.3),
+    createData('Eclair16', 262, 16.0, 24, 6.0),
+    createData('Cupcake17', 305, 3.7, 67, 4.3),
+    createData('Gingerbread18', 356, 16.0, 49, 3.9),
+];
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
 
 const RootPageAccountList = styled(Box)(({ theme }) => ({
     display: "flex",
@@ -26,40 +56,33 @@ const RootPageAccountList = styled(Box)(({ theme }) => ({
     padding: `0 ${theme.spacing(5)} ${theme.spacing(5)} ${theme.spacing(5)}`,
 }));
 
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
+const SearchTextField = styled(MuiTextFeild)(({ theme }) => ({
+    marginBottom: theme.spacing(1),
+}));
 
 function AccountListPage() {
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [rowsSearched, setRowsSearched] = useState(rows);
+    const [searchString, setSearchString] = useState("");
+
+    const filterRows = (searchString) => {
+        if (searchString === "") {
+            return rows;
+        }
+        return rows.filter((row) =>
+            row.name.toLowerCase().includes(searchString.toLowerCase())
+        );
+    };
+
+    useEffect(() => {
+        const filtered = filterRows(searchString);
+        setRowsSearched(filtered);
+    }, [searchString]);
 
     // Avoid a layout jump when reaching the last page with empty rows.
     const emptyRows =
-        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rowsSearched.length) : 0;
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -73,6 +96,22 @@ function AccountListPage() {
     return (
         <RootPageAccountList>
             <Typography variant="h3">Danh sách tài khoản</Typography>
+            <SearchTextField
+                type="text"
+                variant="outlined"
+                component={Paper}
+                placeholder="Tìm kiếm theo tên"
+                value={searchString}
+                onChange={(e) => setSearchString(e.target.value)}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <Search />
+                        </InputAdornment>
+                    ),
+                    style: { fontSize: 18 },
+                }}
+            />
             <TableContainer component={Paper} sx={{ maxHeight: 562, minWidth: 1035, maxWidth: 1035 }}>
                 <Table stickyHeader sx={{ maxWidth: 1200 }}>
                     <TableHead>
@@ -87,8 +126,8 @@ function AccountListPage() {
                     </TableHead>
                     <TableBody>
                         {(rowsPerPage > 0
-                            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            : rows
+                            ? rowsSearched.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            : rowsSearched
                         ).map((row) => (
                             <TableRow key={row.name}>
                                 <TableCell style={{ minWidth: 100 }}>
@@ -124,7 +163,7 @@ function AccountListPage() {
                             <TablePagination
                                 rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
                                 colSpan={6}
-                                count={rows.length}
+                                count={rowsSearched.length}
                                 rowsPerPage={rowsPerPage}
                                 page={page}
                                 onPageChange={handleChangePage}
--- the-duck-mobile-ui/src/pages/Admin/StaffManagement/StaffListPage.jsx
+++ the-duck-mobile-ui/src/pages/Admin/StaffManagement/StaffListPage.jsx
@@ -1,5 +1,6 @@
 import {
     Box,
+    InputAdornment,
     Paper,
     Table,
     TableBody,
@@ -13,34 +14,36 @@ import {
     styled
 } from "@mui/material";
 import TablePaginationActions from "../../../components/TablePaginationActions";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MuiButton from "../../../components/MuiButton";
 import { Link } from "react-router-dom";
 import InfoIcon from '@mui/icons-material/Info';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import { Search } from "@mui/icons-material";
+import MuiTextFeild from "../../../components/MuiTextFeild";
 
 const rows = [
     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Ice cream sandwich1', 237, 9.0, 37, 4.3),
+    createData('Eclair2', 262, 16.0, 24, 6.0),
+    createData('Cupcake3', 305, 3.7, 67, 4.3),
+    createData('Gingerbread3', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt4', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich5', 237, 9.0, 37, 4.3),
+    createData('Eclair6', 262, 16.0, 24, 6.0),
+    createData('Cupcake7', 305, 3.7, 67, 4.3),
+    createData('Gingerbread8', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt9', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich0', 237, 9.0, 37, 4.3),
+    createData('Eclair11', 262, 16.0, 24, 6.0),
+    createData('Cupcake12', 305, 3.7, 67, 4.3),
+    createData('Gingerbread13', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt14', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich15', 237, 9.0, 37, 4.3),
+    createData('Eclair16', 262, 16.0, 24, 6.0),
+    createData('Cupcake17', 305, 3.7, 67, 4.3),
+    createData('Gingerbread18', 356, 16.0, 49, 3.9),
 ];
 
 function createData(name, calories, fat, carbs, protein) {
@@ -54,13 +57,33 @@ const RootPageStaffList = styled(Box)(({ theme }) => ({
     padding: `0 ${theme.spacing(5)} ${theme.spacing(5)} ${theme.spacing(5)}`,
 }));
 
+const SearchTextField = styled(MuiTextFeild)(({ theme }) => ({
+    marginBottom: theme.spacing(1),
+}));
+
 function StaffListPage() {
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [rowsSearched, setRowsSearched] = useState(rows);
+    const [searchString, setSearchString] = useState("");
+
+    const filterRows = (searchString) => {
+        if (searchString === "") {
+            return rows;
+        }
+        return rows.filter((row) =>
+            row.name.toLowerCase().includes(searchString.toLowerCase())
+        );
+    };
+
+    useEffect(() => {
+        const filtered = filterRows(searchString);
+        setRowsSearched(filtered);
+    }, [searchString]);
 
     // Avoid a layout jump when reaching the last page with empty rows.
     const emptyRows =
-        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rowsSearched.length) : 0;
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -74,6 +97,22 @@ function StaffListPage() {
     return (
         <RootPageStaffList>
             <Typography variant="h3">Danh sách nhân viên</Typography>
+            <SearchTextField
+                type="text"
+                variant="outlined"
+                component={Paper}
+                placeholder="Tìm kiếm theo tên"
+                value={searchString}
+                onChange={(e) => setSearchString(e.target.value)}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <Search />
+                        </InputAdornment>
+                    ),
+                    style: { fontSize: 18 },
+                }}
+            />
             <TableContainer component={Paper} sx={{ maxHeight: 562, minWidth: 1035, maxWidth: 1035 }}>
                 <Table stickyHeader sx={{ maxWidth: 1200 }}>
                     <TableHead>
@@ -88,8 +127,8 @@ function StaffListPage() {
                     </TableHead>
                     <TableBody>
                         {(rowsPerPage > 0
-                            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            : rows
+                            ? rowsSearched.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            : rowsSearched
                         ).map((row) => (
                             <TableRow key={row.name}>
                                 <TableCell style={{ minWidth: 100 }}>
@@ -125,7 +164,7 @@ function StaffListPage() {
                             <TablePagination
                                 rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
                                 colSpan={6}
-                                count={rows.length}
+                                count={rowsSearched.length}
                                 rowsPerPage={rowsPerPage}
                                 page={page}
                                 onPageChange={handleChangePage}
