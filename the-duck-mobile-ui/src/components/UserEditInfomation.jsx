96a10db6a84c0f511574de6aa4bd0f03cf277f06
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2023-11-13 00:39:41 +0700

    Fix bug Login Page

--- the-duck-mobile-ui/src/components/UserEditInfomation.jsx
+++ the-duck-mobile-ui/src/components/UserEditInfomation.jsx
@@ -6,15 +6,14 @@ import {
   RadioGroup,
   Stack,
 } from "@mui/material";
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 import { useSnackbar } from "notistack";
 import React, { memo } from "react";
 import { editInfo } from "../services/UserService";
 import DialogForm from "./DialogForm";
 import MuiTextFeild from "./MuiTextFeild";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
 
 function UserEditInfomation(props) {
   const { enqueueSnackbar } = useSnackbar();
