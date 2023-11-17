001fe73e5e78cd25801dd8283ab82ee2357f75da
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2023-11-17 13:03:40 +0700

    Create ProductVersionRequest.cs

---
+++ TheDuckMobile_WebAPI/Models/Request/Admin/ProductVersionRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace TheDuckMobile_WebAPI.Models.Request.Admin
+{
+    public class ProductVersionRequest
+    {
+        public double Price { get; set; }
+        public double PromotionPrice { get; set; }
+        public string? ColorId { get; set; }
+        public string? Specification { get; set; }
+        public string[] Images { get; set; }
+        public DateTime ReleaseTime { get; set; }
+
+        public ProductVersionRequest()
+        {
+            Images = new string[0];
+        }
+    }
+}
